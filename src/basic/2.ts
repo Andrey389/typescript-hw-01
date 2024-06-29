0d8aef8ff112f386aa0342bdab109fdfa6125a87
Andrey389 <148715628+Andrey389@users.noreply.github.com>
2024-06-29 15:57:15 +0300

    add base file

---
+++ src/basic/2.ts
@@ -0,0 +1,6 @@
+type Persons = {
+  name: string;
+  age: number;
+};
+
+let person: Persons[] = [{ name: "Max", age: 21 }];
---
+++ src/basic/3.ts
@@ -0,0 +1,8 @@
+let myType: string | number;
+
+myType = "Hello";
+myType = 345;
+
+type myMessage = "enable" | "disable";
+
+let value: myMessage;
